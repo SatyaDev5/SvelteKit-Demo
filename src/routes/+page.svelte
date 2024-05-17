bde9b7001b5e80b8699cdb5886bf12d3a5ad15da
Satya <satya05112003@gmail.com>
2024-05-17 19:47:07 +0530

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import {LightSwitch} from "@skeletonlabs/skeleton";
+</script>
+<div class="container h-full mx-auto flex justify-center items-center gap-4">
+	Hello World
+	<LightSwitch></LightSwitch>
+</div>
